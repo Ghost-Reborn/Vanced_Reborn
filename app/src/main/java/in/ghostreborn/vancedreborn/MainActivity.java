bdee830c99bc41270696dec93a516b0d7e3f1387
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2022-10-14 06:27:34 +0530

    VancedReborn: load youtube url

--- app/src/main/java/in/ghostreborn/vancedreborn/MainActivity.java
+++ app/src/main/java/in/ghostreborn/vancedreborn/MainActivity.java
@@ -3,6 +3,8 @@ package in.ghostreborn.vancedreborn;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView youtubeWebView = findViewById(R.id.youtube_webview);
+        youtubeWebView.setWebViewClient(new WebViewClient());
+        youtubeWebView.getSettings().setJavaScriptEnabled(true);
+        youtubeWebView.loadUrl("https://youtube.com");
+
     }
 }
